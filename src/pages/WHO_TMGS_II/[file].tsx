2212b477c09d83ec1d8186f857a6c8e043afbd87
jeffersonalopes <jeff.lopes@outlook.com>
2025-12-09 19:34:55 -0300

    try to fix WHO_TMS in public

--- src/pages/WHO_TMGS_II/[file].tsx
+++ src/pages/WHO_TMGS_II/[file].tsx
@@ -2,10 +2,12 @@ import { GetServerSideProps } from "next";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { file } = context.params || {};
-  
+
+  // In Next.js, files in the public folder are served without the /public/ prefix
+  // So /public/WHO_TMGS_II/file.pdf should be accessed as /WHO_TMGS_II/file.pdf
   return {
     redirect: {
-      destination: `/public/WHO_TMGS_II/${file}`,
+      destination: `/WHO_TMGS_II/${file}`,
       permanent: false,
     },
   };
